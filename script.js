e6484fe4d1ac4ee5359260a92dd5cb338942c9f8
kumar15nq <kumar15nq@gmail.com>
2024-09-03 16:00:57 +0530

    News Api

---
+++ script.js
@@ -0,0 +1,10 @@
+function fetchData() {
+  fetch(
+    "https://newsapi.org/v2/everything?q=bitcoin&apiKey=2f7239ca14f54c539ce45a"
+  )
+    .then((val) => val.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+fetchData();
